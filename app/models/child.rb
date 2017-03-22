4ef2b0c4d02a96c03624ca8d58112d720f972e08
Jerome Burkard <jerome.burkard@gadz.org>
2017-03-22 15:38:06 +0100

    nav bar improved

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,12 +1,13 @@
 class Child < ApplicationRecord
   belongs_to :user
   has_many :inscriptions, dependent: :destroy
+  has_many :activities, through: :inscriptions
 
-  validates :first_name, presence: {:message => "Veuillez remplir ce champ"}
+  validates :first_name, presence: {:message => "Veuillez remplir le prénom de l'enfant"}
   validates_uniqueness_of :first_name, scope: [:user_id], message: "Vous avez déjà un enfant avec ce prénom"
-  validates :last_name, presence: {:message => "Veuillez remplir ce champ"}
-  validates :sex, presence: {:message => "Veuillez remplir ce champ"}
+  validates :last_name, presence: {:message => "Veuillez remplir le nom de l'enfant"}
+  validates :sex, presence: {:message => "Veuillez remplir le sexe de l'enfant"}
   validates :sex, inclusion: { in: ["Garçon", "Fille"] }
-  validates :birth_date, presence: {:message => "Veuillez remplir ce champ"}
-  validates :school, presence: {:message => "Veuillez remplir ce champ"}
+  validates :birth_date, presence: {:message => "Veuillez remplir la date de naissance"}
+  validates :school, presence: {:message => "Veuillez remplir l'école de l'enfant"}
 end
